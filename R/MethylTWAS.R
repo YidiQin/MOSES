2f47e50ae659f9f9dc9da46768c6560623219c99
YidiQin <yiq22@pitt.edu>
2023-06-08 10:44:20 -0400

    debug

--- R/MethylTWAS.R
+++ R/MethylTWAS.R
@@ -103,11 +103,10 @@ MethylTWAS <- function(example, train.meth.file, train.exp.file, test.meth.file,
   }
   library(limma)
   confounder.var<- paste(unlist(strsplit(confounder, split = ",")),collapse="+")
-  print(confounder.var)
   design <- model.matrix(as.formula(paste0("~0+",as.character(predictor),"+",confounder.var)), data=pheno)
   print(design)
   fit <- lmFit(pred.gene.exp, design)
-  cont.matrix <- makeContrasts(paste0("CasevsControl=",as.character(predictor),"TRUE-",as.character(predictor),"FALSE"), levels=design)
+  cont.matrix <- makeContrasts(as.formula(paste0("CasevsControl=",as.character(predictor),"TRUE-",as.character(predictor),"FALSE")), levels=design)
   fit2 <- contrasts.fit(fit, cont.matrix)
   fit2 <- eBayes(fit2)
   imputed.TWAS <- topTable(fit2, adjust="BH",number = Inf)
