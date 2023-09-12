361202107ec19f5e995c2ef46cbad3304b484716
YidiQin <yiq22@pitt.edu>
2023-09-12 15:05:57 -0400

    Add files via upload

---
+++ R/MethylTWAS_BySummary.R
@@ -0,0 +1,94 @@
+MethylTWAS_BySummary <- function(example, test.meth.file, TWAS = TRUE, pheno.file, phenotype, confounder, output.file.path, core.num = 1) {
+  message("Importing data ...")
+  if(example == TRUE){
+    data(test.meth)
+  }
+  else{
+    temp <- load(file=test.meth.file)
+    test.meth <- get(temp)
+    rm(temp)
+  }
+
+  ##### load coefficient info #####
+  message("Loading coeffient ...")
+  githubURL <- "https://github.com/YidiQin/MethylTWAS/blob/master/data/test.10M.EVA_PR.cvfit.Rdata?raw=true"
+  download.file(githubURL, destfile= "./test.10M.EVA_PR.cvfit.Rdata", mode = "wb")
+  load("./test.10M.EVA_PR.cvfit.Rdata")
+
+  ##### select genes with promoter info and in test data #####
+  inter.gene.list <- names(all.test.cvfit)
+
+  #inter.gene.list <-promoter.range$gene.name[promoter.range$gene.name %in% colnames(test.meth)]
+  #name<- rownames(train.exp)
+  #dup.name<- name[duplicated(name)]
+
+  ##### prediction parameters #####
+  curi<-1
+  #nfolds<-10
+  lambda.rule <- "lambda.min"
+  #enhancer.range=1e7
+  seq.num <- 1:length(inter.gene.list)
+  n <- ncol(test.meth)
+
+  ###### prediction #####
+  library(parallel)
+  message("Predicting expression ...")
+  #pred.gene.exp <- matrix(NA, ncol=n, nrow=length(inter.gene.list))
+  #rownames(pred.gene.exp) <- inter.gene.list
+  #colnames(pred.gene.exp) <- colnames(test.meth)
+  k <- 1
+    print(paste(k,"th.running",sep=""))
+    prediction_BySummary(all.test.cvfit, seq.num, k, inter.gene.list,
+               test.meth, lambda.rule, n, output.file.path, core.num)
+    load(paste(output.file.path,".",k,"th.running.Rdata",sep=""))
+    sub.exp <- sapply(pred.result, function(x) x[[1]])
+    names(sub.exp) <- inter.gene.list[seq.num]
+    var <- sapply(sub.exp, function(x) var(x))
+    bad.pre <- which(var == 0 | is.na(var))
+    sub.exp <- sub.exp[-bad.pre]
+    pred.gene.exp <- as.data.frame(t(matrix(unlist(sub.exp), nrow=length(unlist(sub.exp[1])))))
+    rownames(pred.gene.exp) <- names(sub.exp)
+    colnames(pred.gene.exp) <- colnames(test.meth)
+  pred.gene.exp <- pred.gene.exp
+  save(list=c('pred.gene.exp'), file=paste0(output.file.path,"prediction.Rdata"))
+
+  ###### TWAS #####
+  if(TWAS == FALSE){
+    message("Skip TWAS ...")
+    stop_quietly()
+  }
+  if(example == TRUE){
+    data(pheno)
+  }
+  else{
+    temp4 <- load(file = pheno.file)
+    pheno <- get(temp4)
+    rm(temp4)
+    #pheno <- read.table(pheno.file,sep="\t", header=TRUE)
+  }
+  library(limma)
+  #print('pass000')
+  confounder.var<- paste(unlist(strsplit(confounder, split = ",")),collapse="+")
+  #print('pass00')
+  #formula <- paste0("~0+",as.character(phenotype),"+",confounder.var)
+  #cmd0 <- paste("design <- model.matrix(as.formula(", formula, "), data=pheno)", sep = '')
+  #eval(parse(text = cmd0))
+  design <- model.matrix(as.formula(paste0("~0+",as.character(phenotype),"+",confounder.var)), data=pheno)
+  #print('pass0')
+  fit <- lmFit(pred.gene.exp, design)
+  #print("pass1")
+  a <- paste0(as.character(phenotype),"TRUE")
+  b <- paste0(as.character(phenotype),"FALSE")
+  #cont.matrix <- makeContrasts(paste0("CasevsControl=",a,"-",b), levels=design)
+  #print("pass2")
+  contrast <- paste0("CasevsControl=",a,"-",b)
+  cmd <- paste("cont.matrix <- makeContrasts(", contrast, ", levels = design)", sep ='')
+  eval(parse(text = cmd))
+  #print("pass3")
+  fit2 <- contrasts.fit(fit, cont.matrix)
+  fit2 <- eBayes(fit2)
+  imputed.TWAS <- topTable(fit2, adjust="BH",number = Inf)
+  write.table(imputed.TWAS, paste0(output.file.path,"TWAS.result.txt"),quote=F,sep="\t",col.names = TRUE, row.names = TRUE)
+}
+
+#MethylTWAS(train.meth.file = "/data/Yang.meth.train.txt", test.meth.file = "/ix/ksoyeon/YQ/data/Yang.meth.test.txt", train.exp.file = "/ix/ksoyeon/YQ/data/Yang.exp.train.txt",pheno.file="/ix/ksoyeon/YQ/data/Yang.pheno.txt",output.file.path = "/ix/ksoyeon/YQ/results/test/")
---
+++ R/Prediction_BySummary.R
@@ -0,0 +1,63 @@
+##### A function to predict gene exp for test data using coefficients #####
+
+library(glmnet)
+prediction_BySummary <- function(all.test.cvfit, seq.num, k, inter.gene.list,
+                       #promoter.range, enhancer.range, train.meth.pos.range, train.exp, train.meth,
+                       test.meth, lambda.rule, n, output.file.path, core.num) {
+  sub.gene.list <- inter.gene.list[seq.num] # select a subset of gene
+  pred.result <- mclapply(1:length(sub.gene.list),function(curi) {
+
+    print(paste(curi,"th running",sep=""))
+    #promoter.range1 <- promoter.range[promoter.range$gene.name == sub.gene.list[curi]] # find promoter loc of the tested gene
+    #candi.enhancer.range1<- GRanges(seqnames=seqnames(promoter.range1), IRanges(start=start(ranges(promoter.range1)) -enhancer.range,end=end(ranges(promoter.range1)) + enhancer.range),gene.name=promoter.range1$gene.name) # define enhancer region of the tested gene
+    #probe.id <- unique(queryHits(findOverlaps(train.meth.pos.range, candi.enhancer.range1))) # get loc of the methylation probes overlapped with the enhancer region
+    #probe.name.unique <- unique(c(promoter.probe.name, gene.body.probe.name))
+    #if(length(probe.id)!=0 ) {
+      #y=t(train.exp[rownames(train.exp) ==  sub.gene.list[curi],,drop=FALSE])
+      #x=t(train.meth[probe.id,])
+      #testx = t(test.meth[colnames(x),])
+      probe.id <- intersect(rownames(test.meth), names(all.test.cvfit[[curi]]))
+      if(length(probe.id) == 0) {
+        pred <- NA
+      }
+      else{
+        testx = t(test.meth[probe.id,])
+        #testx = t(test.meth[rownames(all.test.cvfit[[curi]]$beta),])
+        set.seed(1234)
+        testx <- cbind(1,testx)
+        beta <- c(all.test.cvfit[[curi]]["intercept"], all.test.cvfit[[curi]][probe.id])
+        pred  <- testx %*% beta
+        #pred <- predict(all.test.cvfit[[curi]], newx=testx, s="lambda.min", type="response")
+      }
+
+      #x=apply(x,2,as.double)
+      #ny <- ncol(y)
+      #ntesty <- ncol(testy)
+      #if(ny ==1) {
+        #y <- as.numeric(y)
+        #cv.fit <- cv.glmnet(x, y, keep=TRUE, alpha=0.5)
+        #pred <- predict(cv.fit, newx=testx,s=lambda.rule, type="response")
+      #} else {
+        #scvpearson <- rep(0, ny)
+        #cv.fit <-  vector("list", ny)
+        #for(i in 1:ny) {
+          #y.m <- as.numeric(y[,i])
+          #cv.fit[[i]] <- cv.glmnet(x, y.m, keep=TRUE, alpha=0.5)
+          #id<-which(cv.fit[[i]]$lambda == cv.fit[[i]]$lambda.min)
+
+          #if(var(cv.fit[[i]]$fit.preval[,id])!=0) {
+            #scvpearson[i] <- cor(cv.fit[[i]]$fit.preval[,id],y.m, method = "pearson")
+          #} else {
+            #scvpearson[i] <- 0
+          #}
+        #}
+        #ind <- which.max(abs(scvpearson))
+        #pred <- predict(cv.fit[[ind]], newx=testx,s=lambda.rule, type="response")
+      #}
+    #} else {
+      #pred <- rep(0, n)
+    #}
+    list(pred=pred)
+  },mc.cores=core.num)
+  save(pred.result, file=paste(output.file.path,".",k,"th.running.Rdata",sep=""))
+}
