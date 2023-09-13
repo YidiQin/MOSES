e2acae52db5c6b1e2bee7e52301748db5be3bce6
YidiQin <yiq22@pitt.edu>
2023-09-12 20:03:44 -0400

    update

--- R/MethylTWAS_BySummary.R
+++ R/MethylTWAS_BySummary.R
@@ -8,20 +8,6 @@ MethylTWAS_BySummary <- function(example, test.meth.file, TWAS = TRUE, pheno.fil
     test.meth <- get(temp)
     rm(temp)
   }
-  #library(GenomicRanges)
-  #test.meth.pos.range <- MatchPos(test.meth)
-
-  #message("Matching probes ...")
-  ##### find which methylation probes in testing data are also in annotation set as well as training data #####
-  #int_probe <- intersect(rownames(test.meth),rownames(train.meth))
-  #test.meth <- test.meth[rownames(test.meth) %in% int_probe,]
-  #train.meth <- train.meth[rownames(train.meth) %in% int_probe,]
-  #train.meth.pos.range <- train.meth.pos.range[train.meth.pos.range$name %in% rownames(train.meth),]
-
-  ##### load promoter info #####
-  #promoter<-read.delim("/ix/ksoyeon/eQTMs/hg19_promoter.txt")
-  #load("/ix/ksoyeon/YQ/code/MethylTWAS/data/promoter.rda")
-  #promoter.range <- GRanges(seqnames = promoter$chrID, ranges = IRanges(start=promoter$start, end=promoter$end), strand = promoter$strand, gene.name =promoter$gene.name)
 
   ##### load coefficient info #####
   message("Loading coeffient ...")
@@ -32,24 +18,15 @@ MethylTWAS_BySummary <- function(example, test.meth.file, TWAS = TRUE, pheno.fil
   ##### select genes with promoter info and in test data #####
   inter.gene.list <- names(all.test.cvfit)
 
-  #inter.gene.list <-promoter.range$gene.name[promoter.range$gene.name %in% colnames(test.meth)]
-  #name<- rownames(train.exp)
-  #dup.name<- name[duplicated(name)]
-
   ##### prediction parameters #####
   curi<-1
-  #nfolds<-10
   lambda.rule <- "lambda.min"
-  #enhancer.range=1e7
   seq.num <- 1:length(inter.gene.list)
   n <- ncol(test.meth)
 
   ###### prediction #####
   library(parallel)
   message("Predicting expression ...")
-  #pred.gene.exp <- matrix(NA, ncol=n, nrow=length(inter.gene.list))
-  #rownames(pred.gene.exp) <- inter.gene.list
-  #colnames(pred.gene.exp) <- colnames(test.meth)
   k <- 1
     print(paste(k,"th.running",sep=""))
     prediction_BySummary(all.test.cvfit, seq.num, k, inter.gene.list,
@@ -78,31 +55,18 @@ MethylTWAS_BySummary <- function(example, test.meth.file, TWAS = TRUE, pheno.fil
     temp4 <- load(file = pheno.file)
     pheno <- get(temp4)
     rm(temp4)
-    #pheno <- read.table(pheno.file,sep="\t", header=TRUE)
   }
   library(limma)
-  #print('pass000')
   confounder.var<- paste(unlist(strsplit(confounder, split = ",")),collapse="+")
-  #print('pass00')
-  #formula <- paste0("~0+",as.character(phenotype),"+",confounder.var)
-  #cmd0 <- paste("design <- model.matrix(as.formula(", formula, "), data=pheno)", sep = '')
-  #eval(parse(text = cmd0))
   design <- model.matrix(as.formula(paste0("~0+",as.character(phenotype),"+",confounder.var)), data=pheno)
-  #print('pass0')
   fit <- lmFit(pred.gene.exp, design)
-  #print("pass1")
   a <- paste0(as.character(phenotype),"TRUE")
   b <- paste0(as.character(phenotype),"FALSE")
-  #cont.matrix <- makeContrasts(paste0("CasevsControl=",a,"-",b), levels=design)
-  #print("pass2")
   contrast <- paste0("CasevsControl=",a,"-",b)
   cmd <- paste("cont.matrix <- makeContrasts(", contrast, ", levels = design)", sep ='')
   eval(parse(text = cmd))
-  #print("pass3")
   fit2 <- contrasts.fit(fit, cont.matrix)
   fit2 <- eBayes(fit2)
   imputed.TWAS <- topTable(fit2, adjust="BH",number = Inf)
   write.table(imputed.TWAS, paste0(output.file.path,"TWAS.result.txt"),quote=F,sep="\t",col.names = TRUE, row.names = TRUE)
 }
-
-#MethylTWAS(train.meth.file = "/data/Yang.meth.train.txt", test.meth.file = "/ix/ksoyeon/YQ/data/Yang.meth.test.txt", train.exp.file = "/ix/ksoyeon/YQ/data/Yang.exp.train.txt",pheno.file="/ix/ksoyeon/YQ/data/Yang.pheno.txt",output.file.path = "/ix/ksoyeon/YQ/results/test/")
